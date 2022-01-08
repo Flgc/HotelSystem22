1f6c89ead57819c563dce108469c52cb743c9934
Fabio Luis <ml@fabiotecnico1.biz>
2022-01-07 21:18:36 -0300

    First commit - Class Connect

---
+++ HotelSystem22/src/Logic/Connect.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fabio
+ */
+public class Connect {
+    public String db="dbreserves";
+    public String url="jdbc:mysql://localhost/"+db;
+    public String user="fabio";
+    public String pass="mysql2021";          
+    
+    public Connect() {        
+    }
+    
+    public Connection connection(){
+        Connection link=null;
+        try {
+            Class.forName("org.gjt.mm.mysql.Driver");
+            link=DriverManager.getConnection(this.url, this.user, this.pass);
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Connection Error" + e);
+        }
+        return link;
+    }
+}    
