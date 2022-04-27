e2bccded20ab02291c237b10c4fe3bda6a8745b7
Fabio Luis <ml@fabiotecnico1.biz>
2022-04-27 18:46:02 -0300

    Disable or Enable  columns and components on bedrooms class implemented

--- HotelSystem22/src/Presentation/frm_bedrooms.java
+++ HotelSystem22/src/Presentation/frm_bedrooms.java
@@ -17,7 +17,58 @@ public class frm_bedrooms extends javax.swing.JFrame {
     public frm_bedrooms() {
         initComponents();
     }
+    
+    private String action="save";
+    
+    //Disable table colum
+    void  hide_column(){
+         TB_Lista.getColumnModel().getColumn(0).setMaxWidth(0);
+         TB_Lista.getColumnModel().getColumn(0).setMinWidth(0);
+         TB_Lista.getColumnModel().getColumn(0).setPreferredWidth(0);
+    }
+    
+    void componentDisable(){
+        txt_id_br.setVisible(false);
+        txt_numero.setVisible(false);
+        cmb_andar.setVisible(false);
+        txt_descricao.setVisible(false);
+        txt_caracteristica.setVisible(false);
+        txt_valDiaria.setVisible(false);
+        cmb_estado.setVisible(false);
+        cmb_tipoQuarto.setVisible(false);
+        
+        btn_novo.setEnabled(false);
+        btn_salvar.setEnabled(false);
+        btn_cancelar.setEnabled(false);
+        
+        txt_id_br.setText("");
+        txt_numero.setText("");
+        txt_descricao.setText("");
+        txt_caracteristica.setText("");
+        txt_valDiaria.setText("");
+    }
 
+      void componentEnable(){
+        txt_id_br.setVisible(true);
+        txt_numero.setVisible(true);
+        cmb_andar.setVisible(true);
+        txt_descricao.setVisible(true);
+        txt_caracteristica.setVisible(true);
+        txt_valDiaria.setVisible(true);
+        cmb_estado.setVisible(true);
+        cmb_tipoQuarto.setVisible(true);
+        
+        btn_novo.setEnabled(true);
+        btn_salvar.setEnabled(true);
+        btn_cancelar.setEnabled(true);
+        
+        txt_id_br.setText("");
+        txt_numero.setText("");
+        txt_descricao.setText("");
+        txt_caracteristica.setText("");
+        txt_valDiaria.setText("");
+    }  
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -35,23 +86,24 @@ public class frm_bedrooms extends javax.swing.JFrame {
         cmb_andar = new javax.swing.JComboBox<>();
         lbl_descricao = new javax.swing.JLabel();
         jsc_descricao = new javax.swing.JScrollPane();
-        tar_descricao = new javax.swing.JTextArea();
+        txt_descricao = new javax.swing.JTextArea();
         lbl_caracteristica = new javax.swing.JLabel();
         jsc_caracteristica = new javax.swing.JScrollPane();
-        tar_caracteristica = new javax.swing.JTextArea();
+        txt_caracteristica = new javax.swing.JTextArea();
         lbl_valDiaria = new javax.swing.JLabel();
         txt_valDiaria = new javax.swing.JTextField();
         lbl_estado = new javax.swing.JLabel();
-        cmb_disponivel = new javax.swing.JComboBox<>();
+        cmb_estado = new javax.swing.JComboBox<>();
         lbl_tipoQuarto = new javax.swing.JLabel();
         cmb_tipoQuarto = new javax.swing.JComboBox<>();
         btn_novo = new javax.swing.JButton();
-        btn_salvar = new javax.swing.JButton();
         btn_cancelar = new javax.swing.JButton();
+        btn_salvar = new javax.swing.JButton();
+        txt_id_br = new javax.swing.JTextField();
         jpa_listaQuartos = new javax.swing.JPanel();
         lbl_cadQuarto1 = new javax.swing.JLabel();
         jScrollPane3 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        TB_Lista = new javax.swing.JTable();
         txt_pesquisar = new javax.swing.JTextField();
         lbl_pesquisar = new javax.swing.JLabel();
         btn_sair = new javax.swing.JButton();
@@ -78,16 +130,16 @@ public class frm_bedrooms extends javax.swing.JFrame {
         lbl_descricao.setFont(new java.awt.Font("Times New Roman", 0, 11)); // NOI18N
         lbl_descricao.setText("Descrição");
 
-        tar_descricao.setColumns(20);
-        tar_descricao.setRows(5);
-        jsc_descricao.setViewportView(tar_descricao);
+        txt_descricao.setColumns(20);
+        txt_descricao.setRows(5);
+        jsc_descricao.setViewportView(txt_descricao);
 
         lbl_caracteristica.setFont(new java.awt.Font("Times New Roman", 0, 11)); // NOI18N
         lbl_caracteristica.setText("Caracteristicas");
 
-        tar_caracteristica.setColumns(20);
-        tar_caracteristica.setRows(5);
-        jsc_caracteristica.setViewportView(tar_caracteristica);
+        txt_caracteristica.setColumns(20);
+        txt_caracteristica.setRows(5);
+        jsc_caracteristica.setViewportView(txt_caracteristica);
 
         lbl_valDiaria.setFont(new java.awt.Font("Times New Roman", 0, 11)); // NOI18N
         lbl_valDiaria.setText("Valor Diária");
@@ -96,7 +148,7 @@ public class frm_bedrooms extends javax.swing.JFrame {
         lbl_estado.setText("Estado");
         lbl_estado.setPreferredSize(new java.awt.Dimension(28, 13));
 
-        cmb_disponivel.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Disponível", "Ocupado", "Manutenção", " " }));
+        cmb_estado.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Disponível", "Ocupado", "Manutenção", " " }));
 
         lbl_tipoQuarto.setFont(new java.awt.Font("Times New Roman", 0, 11)); // NOI18N
         lbl_tipoQuarto.setText("Tipo Quarto");
@@ -108,13 +160,13 @@ public class frm_bedrooms extends javax.swing.JFrame {
         btn_novo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Files/images/icones/novo.GIF"))); // NOI18N
         btn_novo.setText("Novo");
 
-        btn_salvar.setFont(new java.awt.Font("Times New Roman", 0, 11)); // NOI18N
-        btn_salvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Files/images/icones/cancelar.png"))); // NOI18N
-        btn_salvar.setText("Cancelar");
-
         btn_cancelar.setFont(new java.awt.Font("Times New Roman", 0, 11)); // NOI18N
-        btn_cancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Files/images/icones/salvar.png"))); // NOI18N
-        btn_cancelar.setText("Salvar");
+        btn_cancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Files/images/icones/cancelar.png"))); // NOI18N
+        btn_cancelar.setText("Cancelar");
+
+        btn_salvar.setFont(new java.awt.Font("Times New Roman", 0, 11)); // NOI18N
+        btn_salvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Files/images/icones/salvar.png"))); // NOI18N
+        btn_salvar.setText("Salvar");
 
         javax.swing.GroupLayout jpa_cadQuartosLayout = new javax.swing.GroupLayout(jpa_cadQuartos);
         jpa_cadQuartos.setLayout(jpa_cadQuartosLayout);
@@ -123,7 +175,6 @@ public class frm_bedrooms extends javax.swing.JFrame {
             .addGroup(jpa_cadQuartosLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jpa_cadQuartosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lbl_cadQuarto)
                     .addGroup(jpa_cadQuartosLayout.createSequentialGroup()
                         .addGroup(jpa_cadQuartosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lbl_numero)
@@ -140,7 +191,7 @@ public class frm_bedrooms extends javax.swing.JFrame {
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                     .addComponent(lbl_estado, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addGap(4, 4, 4)
-                                    .addComponent(cmb_disponivel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(cmb_estado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGroup(jpa_cadQuartosLayout.createSequentialGroup()
                                     .addComponent(txt_numero, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addGap(54, 54, 54)
@@ -148,21 +199,26 @@ public class frm_bedrooms extends javax.swing.JFrame {
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                     .addComponent(cmb_andar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addComponent(jsc_descricao, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(jsc_caracteristica, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(jsc_caracteristica, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(txt_id_br, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(lbl_cadQuarto))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jpa_cadQuartosLayout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btn_novo, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btn_cancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btn_salvar, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btn_cancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(54, 54, 54))
         );
         jpa_cadQuartosLayout.setVerticalGroup(
             jpa_cadQuartosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jpa_cadQuartosLayout.createSequentialGroup()
-                .addComponent(lbl_cadQuarto)
+                .addGroup(jpa_cadQuartosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbl_cadQuarto)
+                    .addComponent(txt_id_br, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(15, 15, 15)
                 .addGroup(jpa_cadQuartosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jpa_cadQuartosLayout.createSequentialGroup()
                         .addGroup(jpa_cadQuartosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -184,7 +240,7 @@ public class frm_bedrooms extends javax.swing.JFrame {
                 .addGroup(jpa_cadQuartosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbl_valDiaria)
                     .addComponent(txt_valDiaria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(cmb_disponivel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cmb_estado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lbl_estado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(11, 11, 11)
                 .addGroup(jpa_cadQuartosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -193,8 +249,8 @@ public class frm_bedrooms extends javax.swing.JFrame {
                 .addGap(27, 27, 27)
                 .addGroup(jpa_cadQuartosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btn_novo)
-                    .addComponent(btn_salvar)
-                    .addComponent(btn_cancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btn_cancelar)
+                    .addComponent(btn_salvar, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -203,7 +259,7 @@ public class frm_bedrooms extends javax.swing.JFrame {
         lbl_cadQuarto1.setFont(new java.awt.Font("Times New Roman", 1, 11)); // NOI18N
         lbl_cadQuarto1.setText("Lista de Quartos");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        TB_Lista.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -214,7 +270,7 @@ public class frm_bedrooms extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane3.setViewportView(jTable1);
+        jScrollPane3.setViewportView(TB_Lista);
 
         lbl_pesquisar.setFont(new java.awt.Font("Times New Roman", 0, 11)); // NOI18N
         lbl_pesquisar.setText("Pesquisar");
@@ -349,6 +405,7 @@ public class frm_bedrooms extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable TB_Lista;
     private javax.swing.JButton btn_apagar;
     private javax.swing.JButton btn_buscar;
     private javax.swing.JButton btn_cancelar;
@@ -356,10 +413,9 @@ public class frm_bedrooms extends javax.swing.JFrame {
     private javax.swing.JButton btn_sair;
     private javax.swing.JButton btn_salvar;
     private javax.swing.JComboBox<String> cmb_andar;
-    private javax.swing.JComboBox<String> cmb_disponivel;
+    private javax.swing.JComboBox<String> cmb_estado;
     private javax.swing.JComboBox<String> cmb_tipoQuarto;
     private javax.swing.JScrollPane jScrollPane3;
-    private javax.swing.JTable jTable1;
     private javax.swing.JPanel jpa_cadQuartos;
     private javax.swing.JPanel jpa_listaQuartos;
     private javax.swing.JScrollPane jsc_caracteristica;
@@ -375,8 +431,9 @@ public class frm_bedrooms extends javax.swing.JFrame {
     private javax.swing.JLabel lbl_registros;
     private javax.swing.JLabel lbl_tipoQuarto;
     private javax.swing.JLabel lbl_valDiaria;
-    private javax.swing.JTextArea tar_caracteristica;
-    private javax.swing.JTextArea tar_descricao;
+    private javax.swing.JTextArea txt_caracteristica;
+    private javax.swing.JTextArea txt_descricao;
+    private javax.swing.JTextField txt_id_br;
     private javax.swing.JTextField txt_numero;
     private javax.swing.JTextField txt_pesquisar;
     private javax.swing.JTextField txt_valDiaria;
